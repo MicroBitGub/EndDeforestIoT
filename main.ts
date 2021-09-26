d27742233c4bb1fb86a82e8ec25fffe98e337dfe
MicroBitGub <89656348+MicroBitGub@users.noreply.github.com>
2021-09-26 15:11:31 +0100

    changed transmitted message

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onGesture(Gesture.FreeFall, function () {
-    while (true) {
-        radio.sendString(GPS)
-    }
+    radio.sendString(TreeFallMessage)
+    radio.sendString("" + (Latitude))
+    radio.sendString("" + (Longitude))
 })
-let GPS = ""
+let TreeFallMessage = ""
+let Longitude = 0
+let Latitude = 0
 radio.setGroup(1)
-let Latitude = 52.1818424
-let Longitude = 0.1789449
-let TreeFallMessage = "Tree Fallen"
-GPS = "" + Latitude + Longitude + TreeFallMessage
+Latitude = 52.1818424
+Longitude = 0.1789449
+TreeFallMessage = "Tree Fallen"
+let GPS = "" + Latitude + Longitude
 basic.showString(GPS)
